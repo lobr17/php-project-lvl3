dac0689d4e550b9021327204cbbe7f2e9a4ff711
Konstantin <konpichugin@yandex.ru>
2021-01-18 18:36:51 +0300

    form save in table, withiut input

--- database/migrations/2021_01_14_081824_create_domains_table.php
+++ database/migrations/2021_01_14_081824_create_domains_table.php
@@ -14,7 +14,7 @@ class CreateDomainsTable extends Migration
     public function up()
     {
         Schema::create('domains', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name');
             $table->timestamps();
         });
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+//use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+	return view('domains');
+	
 });
+	//->name('index');
+
+
+Route::post('/', function (Request $request) {
+
+	$domainData = $request->input('domain[name]');
+
+
+	DB::table('domains')->insertGetId(
+	    ['name' => $domainData,
+             'updated_at' => '12:12:12',
+             'created_at' => '11:11:11']
+        );
+
+	return redirect('/')->with('status', 'INSERTED');
+       // return view('/', ['domains' => $domains]);
+	//return view('/', []);      
+});
+//})->name('domains.store');
